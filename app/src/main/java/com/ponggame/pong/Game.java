4ff6fc03af59d440d9cb6723e1c94ef75c46daaa
yarden <hazanyarden@icloud.com>
2018-06-11 14:55:41 +0300

    checking

--- app/src/main/java/com/ponggame/pong/Game.java
+++ app/src/main/java/com/ponggame/pong/Game.java
@@ -1,11 +1,18 @@
 package com.ponggame.pong;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by yarde on 11/06/2018.
@@ -18,6 +25,25 @@ public class Game extends AppCompatActivity{
     private ImageView pong_left;
     private ImageView pong_right;
     private ImageView Ball;
+
+    private int frame_height ;
+    private int pong_left_size ;
+    private int screenWidth ;
+    private int screenHeight ;
+
+    WindowManager wm = getWindowManager();
+    Display display = wm.getDefaultDisplay();
+
+
+
+
+    private Handler handler = new Handler();
+    private Timer timer = new Timer();
+
+
+
+    private boolean action_flg = false ;
+    private boolean start_flg = false ;
 
     //postion
 
@@ -48,18 +74,84 @@ public class Game extends AppCompatActivity{
 
 
         //temporary
-        Tap_To_Start.setVisibility(View.INVISIBLE);
-        pong_left_Y = 500;
+        //Tap_To_Start.setVisibility(View.INVISIBLE);
+        //pong_left_Y = 500;
+
+
 
     }
 
-    public boolean onTouchEvent(MotionEvent player) {
 
-        if (player.getAction() == MotionEvent.ACTION_DOWN) {
+    public void  changePos(){
+
+        if(action_flg == true)
+        {
             pong_left_Y -= 15;
+        } else {
+
+            pong_left_Y += 15;
+
+
         }
 
-        pong_left.setY(pong_left_Y);
+        //if(pong_left_Y<0) pong_left_Y =0;
+
+
+        pong_left.setY(pong_left_Y);}
+
+
+
+
+    public boolean onTouchEvent(MotionEvent player) {
+
+        if(start_flg ==false)
+        {
+
+            start_flg =true;
+
+//            FrameLayout frame = (FrameLayout)findViewById(R.id.frame);
+//            frame_height = frame.getHeight();
+
+            pong_left_Y = (int) pong_left.getY();
+
+            pong_left_size = pong_left.getHeight();
+
+
+            Tap_To_Start.setVisibility(View.GONE);
+
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            changePos();
+                        }
+                    });
+                }
+            },0,20);
+
+
+
+
+
+
+
+        }else {
+            if (player.getAction() == MotionEvent.ACTION_DOWN) {
+                //pong_left_Y -= 15;
+                action_flg = true;
+
+            } else if (player.getAction() == MotionEvent.ACTION_UP)
+            {
+                action_flg = false ;
+            }
+
+
+
+        }
+
+
 
         return true;
     }
