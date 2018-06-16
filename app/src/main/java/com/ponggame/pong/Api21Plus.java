66f3ededd195009c2ab650bf24ffd00b17f5fa3a
Adican12 <adican12@gmail.com>
2018-06-16 15:41:31 +0300

    start animate need to fix sharedprefernce need to fix sound

---
+++ app/src/main/java/com/ponggame/pong/Api21Plus.java
@@ -0,0 +1,25 @@
+package com.ponggame.pong;
+
+import android.annotation.TargetApi;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
+import android.os.Build;
+
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+class Api21Plus {
+
+    public static SoundPool createSoundPool() {
+        // Initialize AudioAttributes.
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+
+        SoundPool soundPool = new SoundPool.Builder()
+                .setAudioAttributes(attributes)
+                .setMaxStreams(7)
+                .build();
+
+        return soundPool;
+    }
+}
